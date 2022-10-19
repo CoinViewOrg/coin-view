ef2f53aabdafdf533a971fc38016a09f12107d10
Karol Rzotki <krzoti@gmail.com>
2022-10-19 20:24:29 +0200

    create basic coin list

---
+++ pages/api/list.tsx
@@ -0,0 +1,11 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { getCoinList } from "@coin-view";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  const data = await getCoinList();
+  res.status(200).json(data);
+}
---
+++ src/types/index.ts
@@ -0,0 +1,36 @@
+export interface CurrencyPriceData {
+  price: number;
+  volume_24h: number;
+  volume_change_24h: number;
+  percent_change_1h: number;
+  percent_change_24h: number;
+  percent_change_7d: number;
+  market_cap: number;
+  market_cap_dominance: number;
+  fully_diluted_market_cap: number;
+  last_updated: Date;
+}
+
+interface Quote {
+  USD: CurrencyPriceData;
+  BTC: CurrencyPriceData;
+}
+
+export interface CoinListItem {
+  id: number;
+  name: string;
+  symbol: string;
+  slug: string;
+  cmc_rank: number;
+  num_market_pairs: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  last_updated: Date;
+  date_added: Date;
+  tags: string[];
+  platform?: any;
+  self_reported_circulating_supply?: any;
+  self_reported_market_cap?: any;
+  quote: Quote;
+}
