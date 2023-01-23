fe41d6a8d9cb308b278ca4154c344103291d9798
Karol Rzotki <krzoti@gmail.com>
2023-01-23 13:25:15 +0100

    add midleware

---
+++ middleware.ts
@@ -0,0 +1,14 @@
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+
+type Environment = "production" | "development" | "other";
+export function middleware(req: NextRequest, ev: NextFetchEvent) {
+  const currentEnv = process.env.NODE_ENV as Environment;
+
+  if (currentEnv === "production") {
+    return NextResponse.redirect(
+      `https://coin-view.herokuapp.com${req.nextUrl.pathname}`,
+      301
+    );
+  }
+  return NextResponse.next();
+}
