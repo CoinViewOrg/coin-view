0a2223d6470f8a0cf4aa324d8939c050eed887bb
Karol Rzotki <krzoti@gmail.com>
2023-01-23 16:09:32 +0100

    initial theme implementaion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import "../styles/globals.css";
+import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import Script from "next/script";
 import Head from "next/head";
@@ -11,7 +11,7 @@ import {
 import styles from "../styles/App.module.css";
 import { useRouter } from "next/router";
 import React from "react";
-import { AppContext, defaultCurrency } from "@coin-view/context";
+import { AppContext, ColorTheme, defaultCurrency } from "@coin-view/context";
 import Image from "next/future/image";
 import { CoinListItem } from "@coin-view/types";
 import { SessionProvider } from "next-auth/react";
@@ -48,6 +48,11 @@ function MyApp({
     setHamburgerMenuOpen(false);
   }, []);
 
+  const [colorTheme, setColorTheme] = React.useState<ColorTheme>("light");
+  React.useEffect(() => {
+    document.documentElement.setAttribute("data-theme", colorTheme);
+  }, [colorTheme]);
+
   return (
     <>
       <Head>
@@ -62,6 +67,11 @@ function MyApp({
         />
         <meta name="author" content="Karol Rzotki" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://use.typekit.net/rta5gsp.css" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Montserrat"
+        />
       </Head>
 
       <Script id="google-tag-manager" strategy="afterInteractive">
@@ -82,6 +92,7 @@ function MyApp({
             notificationsMenuOpen,
             hamburgerMenuOpen,
             favoriteMarketName: pageProps.favoriteMarketName,
+            colorTheme,
           }}
         >
           <div className={styles.container}>
@@ -96,7 +107,7 @@ function MyApp({
               <div className={styles.mainLogo} onClick={() => push("/list")}>
                 <div className={styles.logoContainer}>
                   <Image
-                    src="/logo-square.svg"
+                    src={`/logo-${colorTheme}.png`}
                     alt="logo"
                     fill
                     sizes="15 vmin"
