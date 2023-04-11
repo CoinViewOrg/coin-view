1cd37b5b53a197e8a2f8a5a7ed606e33853d51f0
Patryk Olender <olenderpat@gmail.com>
2023-04-11 18:37:56 +0200

    Password Reset Form IOC

--- pages/reset/[token].tsx
+++ pages/reset/[token].tsx
@@ -4,16 +4,49 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import React from "react";
 import styles from "../../styles/Home.module.css";
 import { checkResetToken } from "src";
+import { useRouter } from "next/router";
 
 const ResetPage: NextPage<{ token: string; validatedToken: boolean }> = ({
   token,
   validatedToken,
 }) => {
   const { t, language } = useCustomTranslation();
+
+  const [resetError, setResetError] = React.useState();
+  const { push, locale } = useRouter();
+
+  const handleSubmit = React.useCallback(
+    async ({
+      newPassword,
+      repeatNewPassword,
+    }: {
+      newPassword?: string;
+      repeatNewPassword?: string;
+    }) => {
+      const response = await fetch("/api/auth/resetpassword", {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          token,
+          newPassword,
+          repeatNewPassword,
+        }),
+      });
+      const { error } = await response.json();
+
+      if (error) {
+        setResetError(error);
+      } else {
+        push(`/login?passr=1`, undefined, { locale: language });
+      }
+    },
+    [language, push, token]
+  );
+
   return (
     <div className={styles.container}>
       {validatedToken ? (
-        <PasswordResetForm token={token} />
+        <PasswordResetForm onSubmit={handleSubmit} error={resetError} />
       ) : (
         <p>{t("bad_token")}</p>
       )}
