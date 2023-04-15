b8027a5d8ec910befb660416d06cf825bf9a9d6c
Patryk Olender <olenderpat@gmail.com>
2023-04-15 17:22:55 +0200

    Change Password Form tests

---
+++ src/client/components/Forms/ChangePasswordForm.test.tsx
@@ -0,0 +1,49 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { ChangePasswordForm } from "@coin-view/client";
+import { I18nextProvider } from "react-i18next";
+import { instanceEN } from "@coin-view/tests-data";
+
+const onSubmit = jest.fn();
+
+describe("Change Password Form", () => {
+  it("Basic render", () => {
+    render(
+      <I18nextProvider i18n={instanceEN}>
+        <ChangePasswordForm onSubmit={onSubmit} />
+      </I18nextProvider>
+    );
+    expect(screen.getByText("Security")).toBeVisible();
+  });
+  it("Password match error render", () => {
+    render(
+      <I18nextProvider i18n={instanceEN}>
+        <ChangePasswordForm onSubmit={onSubmit} error={1} />
+      </I18nextProvider>
+    );
+    const button = screen.getByAltText("security");
+    fireEvent(
+      button,
+      new MouseEvent("click", {
+        bubbles: true,
+      })
+    );
+    expect(screen.getByText("New passwords do not match.")).toBeVisible();
+  });
+  it("Bad current password error render", () => {
+    render(
+      <I18nextProvider i18n={instanceEN}>
+        <ChangePasswordForm onSubmit={onSubmit} error={2} />
+      </I18nextProvider>
+    );
+    const button = screen.getByAltText("security");
+    fireEvent(
+      button,
+      new MouseEvent("click", {
+        bubbles: true,
+      })
+    );
+    expect(
+      screen.getByText("You provided wrong current password.")
+    ).toBeVisible();
+  });
+});
