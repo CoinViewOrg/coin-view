6a2fcee232468303e5c9960b5b50dafd646d45b2
Patryk Olender <olenderpat@gmail.com>
2023-04-16 23:17:30 +0200

    List switcher tests

---
+++ src/client/components/ListSwitcher/ListSwitcher.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+import { ListSwitcher } from "./ListSwitcher";
+import { I18nextProvider } from "react-i18next";
+import { instanceEN } from "@coin-view/mocks";
+import { useRouter } from "next/router";
+
+jest.mock("next/router", () => ({
+  __esModule: true,
+  useRouter: jest.fn(),
+}));
+
+const mockRouter = {
+  pathname: "/",
+  replace: jest.fn(),
+};
+
+describe("List navigation", () => {
+  it("Basic render", () => {
+    (useRouter as jest.Mock).mockReturnValue(mockRouter);
+    render(
+      <I18nextProvider i18n={instanceEN}>
+        <ListSwitcher />
+      </I18nextProvider>
+    );
+    expect(screen.getByText("All")).toBeVisible();
+    expect(screen.getByText("Favourites")).toBeVisible();
+  });
+});
